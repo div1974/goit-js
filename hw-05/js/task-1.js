b097700ca57e28ee6191cac96bc136ec7473996a
div1974 <div@ukr.net>
2020-03-26 16:20:51 +0200

    hw-05

---
+++ hw-05/js/task-1.js
@@ -0,0 +1,27 @@
+const Account = function({ login, email }) {
+  this.login = login;
+  this.email = email;
+
+  Account.prototype.getInfo = function() {
+    console.log(`login: ${this.login}, email: ${this.email}`);
+  };
+};
+
+
+
+const mango = new Account({
+  login: "Mangozedog",
+  email: "mango@dog.woof"
+});
+
+console.log(Account.prototype.getInfo); // function
+
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: "Poly",
+  email: "poly@mail.com"
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
